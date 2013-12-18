abd47b8037fe7daf976b25e458e98a1d183da477
ulber <olli.saarikivi@iki.fi>
2013-12-18 13:32:06 +0200

    And now with all the files...

---
+++ Reitit/MapFrame.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Maps.Controls;
+using System.Windows.Media;
+
+namespace Reitit
+{
+    public partial class MapFrame : PhoneApplicationFrame
+    {
+        private TranslateTransform _mapTransform = new TranslateTransform();
+
+        public Map Map { get; private set; }
+
+        public MapFrame()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            base.OnContentChanged(oldContent, newContent);
+
+            var oldPage = oldContent as FrameworkElement;
+            if (oldPage != null)
+            {
+                oldPage.SizeChanged -= ContentSizeChanged;
+            }
+
+            var page = newContent as FrameworkElement;
+            if (page != null)
+            {
+                page.SizeChanged += ContentSizeChanged;
+                UpdateMapTransform(page.ActualHeight);
+            }
+            else
+            {
+                throw new NotImplementedException("MapFrame does not support non-FrameworkElement content");
+            }
+        }
+
+        private void ContentSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateMapTransform((sender as FrameworkElement).ActualHeight);
+        }
+
+        private void UpdateMapTransform(double contentHeight)
+        {
+            _mapTransform.Y = -contentHeight / 2;
+        }
+
+        private void Map_Loaded(object sender, RoutedEventArgs e)
+        {
+            Map = sender as Map;
+            Map.RenderTransform = _mapTransform;
+        }
+    }
+}
---
+++ Reitit/MapFramePage.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Phone.Controls;
+using Microsoft.Phone.Maps.Controls;
+using Microsoft.Phone.Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Reitit
+{
+    public class MapFramePage : PhoneApplicationPage
+    {
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            SystemTray.IsVisible = true;
+            SystemTray.Opacity = 0;
+            // The foreground color is contrasting if the map theme matches the phone theme
+            if ((Visibility)Application.Current.Resources["PhoneLightThemeVisibility"] == Visibility.Visible)
+            {
+                SystemTray.ForegroundColor = (Color)Application.Current.Resources["PhoneForegroundColor"];
+            }
+            else
+            {
+                SystemTray.ForegroundColor = (Color)Application.Current.Resources["PhoneBackgroundColor"];
+            }
+        }
+    }
+}
