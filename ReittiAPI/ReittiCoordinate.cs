6edff2f881c7a675dd8d3e775fb7813f996de451
ulber <olli.saarikivi@iki.fi>
2014-01-16 11:42:13 +0200

    ReititCycling work

--- ReittiAPI/ReittiCoordinate.cs
+++ ReittiAPI/ReittiCoordinate.cs
@@ -21,22 +21,30 @@ namespace ReittiAPI
     [DataContract]
     public class ReittiCoordinate
     {
-        public ReittiCoordinate(double latitude, double longitude)
+        public ReittiCoordinate(double latitude, double longitude, double? altitude = null)
         {
             Latitude = latitude;
             Longitude = longitude;
+            Altitude = altitude;
         }
 
         public static bool TryParse(string s, out ReittiCoordinate coordinate)
         {
             var substrings = s.Split(',');
-            double latitude, longitude;
+            double latitude, longitude, altitude;
             if (substrings.Length == 2 &&
                 double.TryParse(substrings[0], NumberStyles.Any, CultureInfo.InvariantCulture, out latitude) &&
                 double.TryParse(substrings[1], NumberStyles.Any, CultureInfo.InvariantCulture, out longitude))
             {
                 coordinate = new ReittiCoordinate(latitude, longitude);
                 return true;
+            }
+            else if (substrings.Length == 3 &&
+                double.TryParse(substrings[0], NumberStyles.Any, CultureInfo.InvariantCulture, out latitude) &&
+                double.TryParse(substrings[1], NumberStyles.Any, CultureInfo.InvariantCulture, out longitude) &&
+                double.TryParse(substrings[2], NumberStyles.Any, CultureInfo.InvariantCulture, out altitude))
+            {
+
             }
             coordinate = null;
             return false;
@@ -51,22 +59,31 @@ namespace ReittiAPI
                     double.Parse(substrings[0], CultureInfo.InvariantCulture),
                     double.Parse(substrings[1], CultureInfo.InvariantCulture));
             }
+            else if (substrings.Length == 3)
+            {
+                return new ReittiCoordinate(
+                    double.Parse(substrings[0], CultureInfo.InvariantCulture),
+                    double.Parse(substrings[1], CultureInfo.InvariantCulture),
+                    double.Parse(substrings[2], CultureInfo.InvariantCulture));
+            }
             throw new FormatException("Not enough parts");
         }
 
         public static implicit operator GeoCoordinate(ReittiCoordinate c)
         {
-            return new GeoCoordinate(c.Latitude, c.Longitude);
+            return c.Altitude.HasValue ? 
+                new GeoCoordinate(c.Latitude, c.Longitude, c.Altitude.Value) :
+                new GeoCoordinate(c.Latitude, c.Longitude);
         }
 
         public static explicit operator ReittiCoordinate(GeoCoordinate c)
         {
-            return new ReittiCoordinate(c.Latitude, c.Longitude);
+            return new ReittiCoordinate(c.Latitude, c.Longitude, c.Altitude);
         }
 
         public static explicit operator ReittiCoordinate(Geocoordinate c)
         {
-            return new ReittiCoordinate(c.Latitude, c.Longitude);
+            return new ReittiCoordinate(c.Latitude, c.Longitude, c.Altitude);
         }
 
         public override string ToString()
@@ -85,5 +102,8 @@ namespace ReittiAPI
 
         [DataMember]
         public double Longitude;
+
+        [DataMember]
+        public double? Altitude;
     }
 }
